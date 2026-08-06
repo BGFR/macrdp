2e50fe75a5a475d07f934575f957b6a01789f7b8
Clint Christopher Canada <clintcan@gmail.com>
2026-08-06 20:10:58 +0800

    chore: remove orphaned vendor/ironrdp-rdpeusb/fuzz (crate retired at the a5d1c682 pin bump)

--- vendor/ironrdp-rdpeusb/fuzz/target/aarch64-apple-darwin/release/deps/client_pdu-7cbcad3b5d6518ac.d
+++
@@ -1,5 +0,0 @@
-/Users/clintcan/Documents/Projects/Misc/macrdp/vendor/ironrdp-rdpeusb/fuzz/target/aarch64-apple-darwin/release/deps/client_pdu-7cbcad3b5d6518ac.d: fuzz_targets/client_pdu.rs
-
-/Users/clintcan/Documents/Projects/Misc/macrdp/vendor/ironrdp-rdpeusb/fuzz/target/aarch64-apple-darwin/release/deps/client_pdu-7cbcad3b5d6518ac: fuzz_targets/client_pdu.rs
-
-fuzz_targets/client_pdu.rs:
